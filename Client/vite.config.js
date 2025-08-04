a7d3f1d6446221ef117260306f58ca3a46d8e49b
Developer866 <opeyemijoseph866@gmail.com>
2025-08-04 18:34:30 +0100

    local network preview

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   server: {
+    host: true, // this allows access from local network
+    port: 5173, // optional, you can choose another port
+  },
 })
